9c4941048785a2d3e24185689d0feb561cac528a
NathanMoes <Nathanmoes7@gmail.com>
2022-05-21 11:42:24 -0700

    Add in norm distro function

--- temp.py
+++ temp.py
@@ -3,14 +3,27 @@ import numpy as np
 import math
 
 
-if __name__ == "__main__":
-    train = []
-    test = []
-    with open('test.csv', newline='\n') as csvfile:
-        csv_reader = csv.reader(csvfile, delimiter=',')
-        for row in csv_reader:
-            test.append(row)
-    with open('train.csv', newline='\n') as csvfile:
+
+
+def get_norm_dist(x, u, a):  # gets the norm distribution for the passed in data
+    part_one = 0.0  # broken into 2 parts just to make it more readable and easier
+    part_two = 0.0  # 2nd part
+    part_one = (1 / (math.sqrt(2 * math.pi)))
+    part_two = -1 * (((x-u) * (x-u))/(2*a*a))
+    result = part_one * math.exp(part_two)  # raise e to power of part two
+    if result < 0.0001:  # to not divide by 0 etc
+        return 0.0001
+    return result
+
+
+if __name__ == "__main__":  # main function call
+    train = [] # traindata
+    test = [] # testdata
+    with open('test.csv', newline='\n') as csvfile:  # read in from test.csv for test data
+        csv_reader = csv.reader(csvfile, delimiter=',') # reads in 
+        for row in csv_reader:  # append stuff from each row/data point
+            test.append(row)  # append
+    with open('train.csv', newline='\n') as csvfile: # same as above but for train
         csv_reader = csv.reader(csvfile, delimiter=',')
         for row in csv_reader:
             train.append(row)
