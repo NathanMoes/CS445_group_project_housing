30514460315f22717d09bb86925c35d88760fcd7
NathanMoes <91927252+NathanMoes@users.noreply.github.com>
2022-05-21 11:46:26 -0700

    Update temp.py

--- temp.py
+++ temp.py
@@ -16,6 +16,43 @@ def get_norm_dist(x, u, a):  # gets the norm distribution for the passed in data
     return result
 
 
+# ignore for now, just has stuff in it from my prog 2. Just pasted in code
+def get_standard(input_array):  # gets the standard distribution for the data passed in
+    mean = np.zeros((1, 58))  # mean
+    std = np.zeros((1, 58))  # standard dev
+    mean_not = np.zeros((1, 58))  # same as above but for the not spam class
+    std_not = np.zeros((1, 58))
+    type_of = 0  # spam or not spam checker
+    hold = 0.0  # for doing squares, just like it to be self * self.
+    for j in range(0, len(input_array)):  # for all rows/data in input calculate
+        # mean += input_array[j][0]
+        type_of = int(input_array[j][57])
+        if type_of == 1:  # check what class
+            for h in range(0, 58):
+                mean[0][h] += float(input_array[j][h])  # add in each datum for part
+        else:
+            for h in range(0, 58):
+                mean_not[0][h] += float(input_array[j][h])
+    for index in range(0, 58):  # calc mean by doing sum of above div the data number of
+        mean[0][index] /= len(input_array)
+        mean_not[0][index] /= len(input_array)
+    for j in range(0, len(input_array)):  # calc standard deviation
+        for h in range(0, 58):  # for spam
+            hold = (float(input_array[j][h]) - float(mean[0][h]))
+            std[0][h] += (hold * hold)
+        for h in range(0, 58):  # for not spam
+            hold = (float(input_array[j][h]) - float(mean_not[0][h]))
+            std_not[0][h] += (hold * hold)
+    for index in range(0, 58):  # finish above calc
+        std[0][index] = math.sqrt((std[0][index]) / len(input_array))
+        if std[0][index] == 0 or std[0][index] == 0.0:
+            std[0][index] = 0.0001
+        std_not[0][index] = math.sqrt((std_not[0][index]) / len(input_array))
+        if std_not[0][index] == 0 or std_not[0][index] == 0.0:
+            std_not[0][index] = 0.0001
+    return mean, std, mean_not, std_not  # return arrays of new calc data
+
+
 if __name__ == "__main__":  # main function call
     train = [] # traindata
     test = [] # testdata
