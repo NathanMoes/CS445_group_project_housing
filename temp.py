1120a1f4d14c1e6dca42c8a66a1c0616510127dd
NathanMoes <Nathanmoes7@gmail.com>
2022-05-21 11:38:50 -0700

    read in from csv

--- temp.py
+++ temp.py
@@ -1,5 +1,21 @@
+import csv
+import numpy as np
+import math
 
 
-
-if __name__ == "__main__"
+if __name__ == "__main__":
+    train = []
+    test = []
+    with open('test.csv', newline='\n') as csvfile:
+        csv_reader = csv.reader(csvfile, delimiter=',')
+        for row in csv_reader:
+            test.append(row)
+    with open('train.csv', newline='\n') as csvfile:
+        csv_reader = csv.reader(csvfile, delimiter=',')
+        for row in csv_reader:
+            train.append(row)
+    print("testData")
+    print(test[1])
+    print("trainData")
+    print(train[1])
     print("testing")
