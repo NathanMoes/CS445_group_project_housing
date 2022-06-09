a966562fead08ee2d0cf4c9f9564fb3541c0b44e
NathanMoes <91927252+NathanMoes@users.noreply.github.com>
2022-06-09 12:36:08 -0700

    did RMSE

--- temp.py
+++ temp.py
@@ -24,7 +24,7 @@ def get_norm_dist(x, u, a):  # gets the norm distribution for the passed in data
     part_one = 0.0  # broken into 2 parts just to make it more readable and easier
     part_two = 0.0  # 2nd part
     part_one = (1 / (math.sqrt(2 * math.pi)))
-    part_two = -1 * (((x-u) * (x-u))/(2*a*a))
+    part_two = -1 * (((x - u) * (x - u)) / (2 * a * a))
     result = part_one * math.exp(part_two)  # raise e to power of part two
     if result < 0.0001:  # to not divide by 0 etc
         return 0.0001
@@ -42,7 +42,7 @@ def get_prob(train_v, input_v, class_type, feature_type):
         for rows_in in range(1, number_of):
             if get_class(train_v[rows_in][80]) == class_type:
                 count += 1
-        probability = float(count/number_of)
+        probability = float(count / number_of)
         return probability
     if feature_type == (3 or 4 or 27 or 35 or 37 or 38 or 39 or 44 or 47 or 45 or 46 or 63 or 67 or 72 or 68 or 69
                         or 70 or 71 or 76 or 77):
@@ -53,7 +53,7 @@ def get_prob(train_v, input_v, class_type, feature_type):
         mean /= 728
         hold = (float(input_v[feature_type]) - float(mean))
         std += (hold * hold)
-        std = math.sqrt(std/count_in_class)
+        std = math.sqrt(std / count_in_class)
         if std < 0.0001:
             std = 0.0001
         return get_norm_dist(float(input_v[feature_type]), mean, std)
@@ -62,7 +62,7 @@ def get_prob(train_v, input_v, class_type, feature_type):
             count_in_class += 1
             if train_v[row_t][feature_type] == input_v[feature_type]:
                 count += 1
-    probability = float(count/count_in_class)
+    probability = float(count / count_in_class)
     if probability <= 0.0001:
         return 0.0001
     return probability
@@ -70,13 +70,13 @@ def get_prob(train_v, input_v, class_type, feature_type):
 
 if __name__ == "__main__":  # main function call
     conf_mat = np.zeros((6, 6))
-    train = [] # traindata
-    test = [] # testdata
+    train = []  # traindata
+    test = []  # testdata
     with open('test.csv', newline='\n') as csvfile:  # read in from test.csv for test data
-        csv_reader = csv.reader(csvfile, delimiter=',') # reads in
+        csv_reader = csv.reader(csvfile, delimiter=',')  # reads in
         for row in csv_reader:  # append stuff from each row/data point
             test.append(row)  # append
-    with open('train.csv', newline='\n') as csvfile: # same as above but for train
+    with open('train.csv', newline='\n') as csvfile:  # same as above but for train
         csv_reader = csv.reader(csvfile, delimiter=',')
         for row in csv_reader:
             train.append(row)
@@ -102,12 +102,12 @@ if __name__ == "__main__":  # main function call
         max_index = 0
         prob = np.zeros((6, 1))
         for feature in range(0, 80):
-            prob[0] += math.log(get_prob(train, train[iteration+half_point], 0, feature))
-            prob[1] += math.log(get_prob(train, train[iteration+half_point], 1, feature))
-            prob[2] += math.log(get_prob(train, train[iteration+half_point], 2, feature))
-            prob[3] += math.log(get_prob(train, train[iteration+half_point], 3, feature))
-            prob[4] += math.log(get_prob(train, train[iteration+half_point], 4, feature))
-            prob[5] += math.log(get_prob(train, train[iteration+half_point], 5, feature))
+            prob[0] += math.log(get_prob(train, train[iteration + half_point], 0, feature))
+            prob[1] += math.log(get_prob(train, train[iteration + half_point], 1, feature))
+            prob[2] += math.log(get_prob(train, train[iteration + half_point], 2, feature))
+            prob[3] += math.log(get_prob(train, train[iteration + half_point], 3, feature))
+            prob[4] += math.log(get_prob(train, train[iteration + half_point], 4, feature))
+            prob[5] += math.log(get_prob(train, train[iteration + half_point], 5, feature))
         for check in range(0, 6):
             if prob[check] > maximum:
                 maximum = prob[check]
@@ -117,18 +117,18 @@ if __name__ == "__main__":  # main function call
             # print("predicted sale price is less than 50k")
             result = float(train[iteration + half_point][80])
             median_zero.append(result)
-            if get_class(train[iteration+half_point][80]) == 0:
+            if get_class(train[iteration + half_point][80]) == 0:
                 # print("CORRECT")
                 correct_count += 1
                 conf_mat[0][0] += 1
             else:
-                conf_mat[get_class(train[iteration+half_point][80])][0] += 1
+                conf_mat[get_class(train[iteration + half_point][80])][0] += 1
                 # print("INCORRECT")
         if max_index == 1:
             result = float(train[iteration + half_point][80])
             # print("predicted sale price is between 50k and 100k")
             median_one.append(result)
-            if get_class(train[iteration+half_point][80]) == 1:
+            if get_class(train[iteration + half_point][80]) == 1:
                 # print("CORRECT")
                 correct_count += 1
                 conf_mat[1][1] += 1
@@ -139,7 +139,7 @@ if __name__ == "__main__":  # main function call
             result = float(train[iteration + half_point][80])
             median_two.append(result)
             # print("predicted sale price is between 100k and 150k")
-            if get_class(train[iteration+half_point][80]) == 2:
+            if get_class(train[iteration + half_point][80]) == 2:
                 # print("CORRECT")
                 correct_count += 1
                 conf_mat[2][2] += 1
@@ -150,7 +150,7 @@ if __name__ == "__main__":  # main function call
             result = float(train[iteration + half_point][80])
             median_three.append(result)
             # print("predicted sale price is between 150k and 200k")
-            if get_class(train[iteration+half_point][80]) == 3:
+            if get_class(train[iteration + half_point][80]) == 3:
                 # print("CORRECT")
                 correct_count += 1
                 conf_mat[3][3] += 1
@@ -161,7 +161,7 @@ if __name__ == "__main__":  # main function call
             result = float(train[iteration + half_point][80])
             median_four.append(result)
             # print("predicted sale price is between 200k and 250k")
-            if get_class(train[iteration+half_point][80]) == 4:
+            if get_class(train[iteration + half_point][80]) == 4:
                 # print("CORRECT")
                 correct_count += 1
                 conf_mat[4][4] += 1
@@ -174,7 +174,7 @@ if __name__ == "__main__":  # main function call
             median_five.append(result)
             # print("Medians 5: " + str(statistics.median(median_five)))
             # print("predicted sale price is 250k+")
-            if get_class(train[iteration+half_point][80]) == 5:
+            if get_class(train[iteration + half_point][80]) == 5:
                 # print("CORRECT")
                 correct_count += 1
                 conf_mat[5][5] += 1
@@ -182,25 +182,59 @@ if __name__ == "__main__":  # main function call
                 # print("INCORRECT")
                 conf_mat[get_class(train[iteration + half_point][80])][5] += 1
             # prob[0] += math.log(get_prob(train, test[iteration], 0, feature))
-    
+
     zero = 0
     one = 0
     two = 0
     three = 0
     four = 0
     five = 0
+    # zero_med = statistics.median(median_one)
+    one_med = statistics.median(median_one)
+    two_med = statistics.median(median_two)
+    three_med = statistics.median(median_three)
+    four_med = statistics.median(median_four)
+    five_med = statistics.median(median_five)
     for calc in range(0, 6):
-        zero += conf_mat[0][calc]
-        one += conf_mat[1][calc]
-        two += conf_mat[2][calc]
-        three += conf_mat[3][calc]
-        four += conf_mat[4][calc]
-        five += conf_mat[5][calc]
+        zero += conf_mat[calc][0]
+        one += conf_mat[calc][1]
+        two += conf_mat[calc][2]
+        three += conf_mat[calc][3]
+        four += conf_mat[calc][4]
+        five += conf_mat[calc][5]
     sum_num = 0.0
-    for calc in range(one):
-        sum_num += median_one[calc] - 2
+    for calc in range(0, int(one)):
+        store = median_one[calc] - one_med
+        sum_num += (store * store)
+    sum_num /= one
+    print("RMSE of one class: " + str(np.sqrt(sum_num)))
+    sum_num = 0.0
+    for calc in range(0, int(two)):
+        store = median_two[calc] - two_med
+        sum_num += (store * store)
+    sum_num /= two
+    print("RMSE of two class: " + str(np.sqrt(sum_num)))
+    sum_num = 0.0
+    for calc in range(0, int(three)):
+        store = median_three[calc] - three_med
+        sum_num += (store * store)
+    sum_num /= three
+    print("RMSE of three class: " + str(np.sqrt(sum_num)))
+    sum_num = 0.0
+    for calc in range(0, int(four)):
+        store = median_four[calc] - four_med
+        sum_num += (store * store)
+    sum_num /= four
+    print("RMSE of three class: " + str(np.sqrt(sum_num)))
+    sum_num = 0.0
+    for calc in range(0, int(five)):
+        store = median_five[calc] - five_med
+        sum_num += (store * store)
+    sum_num /= five
+    print("RMSE of three class: " + str(np.sqrt(sum_num)))
+    sum_num = 0.0
     # means_square_error_rooted = 
-    
+
     print("Correct % is: " + str(correct_count / 729))
     print(conf_mat)
     print(median_zero)
